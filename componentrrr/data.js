bd8b6cfa7157a4f76d046176818df6b5eefb5e46
Ranjeet Singh <107486727+Rnjeetsingh@users.noreply.github.com>
2022-09-30 10:20:21 +0530

    Task Completed

---
+++ componentrrr/data.js
@@ -0,0 +1,151 @@
+function getData(){   {jackets:[{},{}]}
+    return `{
+jackets: [
+    {
+        "id": "j1",
+        "name": "Sap Green Twill Jacket",
+        "price": "3049",
+        "image1": "https://images.bewakoof.com/t540/sap-green-twill-jacket-aw-21-367105-1637319099-1.jpg",
+        "image2": "https://images.bewakoof.com/t540/sap-green-twill-jacket-aw-21-367105-1637319102-2.jpg",
+        "image3": "https://images.bewakoof.com/t540/sap-green-twill-jacket-aw-21-367105-1637319106-3.jpg",
+        "image4": "https://images.bewakoof.com/t540/sap-green-twill-jacket-aw-21-367105-1637319109-4.jpg",
+        "image5": "https://images.bewakoof.com/t540/sap-green-twill-jacket-aw-21-367105-1637319113-5.jpg",
+        "desc": "Make a remarkable appearance with this Men's Fashion Twill Winter Jacket. Style it with all your casual outfits and loafers or sports shoes for an effortlessly stylish look.\nCountry of Origin : India",
+        "tribe": "120",
+        "discount": "57"
+    },
+    {
+        "id": "j2",
+        "name": "Light Pink Twill Jacket",
+        "price": "3049",
+        "image1": "https://images.bewakoof.com/t540/l-pink-twill-jacket-aw-21-367104-1637147387-1.jpg",
+        "image2": "https://images.bewakoof.com/t540/l-pink-twill-jacket-aw-21-367104-1637147391-2.jpg",
+        "image3": "https://images.bewakoof.com/t540/l-pink-twill-jacket-aw-21-367104-1637147394-3.jpg",
+        "image4": "https://images.bewakoof.com/t540/l-pink-twill-jacket-aw-21-367104-1637147398-4.jpg",
+        "image5": "https://images.bewakoof.com/t540/l-pink-twill-jacket-aw-21-367104-1637147401-5.jpg",
+        "desc": "Leave no room for chills in this Light Pink Twill Jacket. Style this jacket with a pair of denims and sneakers for a stylish look. Ditch the heater, your outfit's gonna create enough heat!",
+        "tribe": "80",
+        "discount": "67"
+    },
+    {
+        "id": "j3",
+        "name": "Lemon Curry Twill Jacket",
+        "price": "3049",
+        "image1": "https://images.bewakoof.com/t540/lemon-curry-twill-jacket-aw-21-391580-1639043325-1.jpg",
+        "image2": "https://images.bewakoof.com/t540/lemon-curry-twill-jacket-aw-21-391580-1639043329-2.jpg",
+        "image3": "https://images.bewakoof.com/t540/lemon-curry-twill-jacket-aw-21-391580-1639043332-3.jpg",
+        "image4": "https://images.bewakoof.com/t540/lemon-curry-twill-jacket-aw-21-391580-1639043336-4.jpg",
+        "image5": "https://images.bewakoof.com/t540/lemon-curry-twill-jacket-aw-21-391580-1639043339-5.jpg",
+        "desc": "Regular fit - Falls loosely on the body Twill,100% Cotton Soft & Durable",
+        "tribe": "80",
+        "discount": "67"
+    },
+    {
+        "id": "j4",
+        "name": "Black Plus Size Puffer",
+        "price": "3649",
+        "image1": "https://images.bewakoof.com/t540/black-plus-size-puffer-jacket-364914-1642433427-1.jpg",
+        "image2": "https://images.bewakoof.com/t540/black-plus-size-puffer-jacket-364914-1642433431-2.jpg",
+        "image3": "https://images.bewakoof.com/t540/black-plus-size-puffer-jacket-364914-1642433435-3.jpg",
+        "image4": "https://images.bewakoof.com/t540/black-plus-size-puffer-jacket-364914-1642433439-4.jpg",
+        "image5": "https://images.bewakoof.com/t540/black-plus-size-puffer-jacket-364914-1642433443-5.jpg",
+        "desc": "Flaunt your hot sassy looks in this Black Mens Plus Size Puffer Jacket. Be upbeat, match this puffer jacket with funky bag, trousers and some warm comfy boots and level up your style game.",
+        "tribe": "100",
+        "discount": "64"
+    },
+    {
+        "id": "j5",
+        "name": "Yellow Reverssible Puffer Jacket",
+        "price": "3349",
+        "image1": "https://images.bewakoof.com/t540/royal-blue-gold-sleeveless-puffer-jacket-aw-21-410313-1638435504-1.jpg",
+        "image2": "https://images.bewakoof.com/t540/royal-blue-gold-sleeveless-puffer-jacket-aw-21-410313-1638435507-2.jpg",
+        "image3": "https://images.bewakoof.com/t540/royal-blue-gold-sleeveless-puffer-jacket-aw-21-410313-1638435511-3.jpg",
+        "image4": "https://images.bewakoof.com/t540/royal-blue-gold-sleeveless-puffer-jacket-aw-21-410313-1638435514-4.jpg",
+        "image5": "https://images.bewakoof.com/t540/royal-blue-gold-sleeveless-puffer-jacket-aw-21-410313-1638435518-5.jpg",
+        "desc": "An epitome of style and comfort this Blue Color Block Puffer Jacket is a must-have Style it with jeans and same coloured sneakers for the most intensifying appearance.",
+        "tribe": "100",
+        "discount": "61"
+    },
+    {
+        "id": "j6",
+        "name": "Camo Bomber Jackets",
+        "price": "3349",
+        "image1": "https://images.bewakoof.com/t540/olive-camo-bomber-jackets-aw-21-368765-1639646268-1.jpg",
+        "image2": "https://images.bewakoof.com/t540/olive-camo-bomber-jackets-aw-21-368765-1639646272-2.jpg",
+        "image3": "https://images.bewakoof.com/t540/olive-camo-bomber-jackets-aw-21-368765-1639646275-3.jpg",
+        "image4": "https://images.bewakoof.com/t540/olive-camo-bomber-jackets-aw-21-368765-1639646279-4.jpg",
+        "image5": "https://images.bewakoof.com/t540/olive-camo-bomber-jackets-aw-21-368765-1639646282-5.jpg",
+        "desc": "Regular fit - Fits just right - not too tight, not too loose.100% Polyester Light Weight, Soft & Durab",
+        "tribe": "80",
+        "discount": "70"
+    },
+    {
+        "id": "j7",
+        "name": "Black Puffer Hoodie Jacket",
+        "price": "₹3299",
+        "image1": "https://images.bewakoof.com/t540/black-puffer-jacket-aw-21-364956-1634190921-1.jpg",
+        "image2": "https://images.bewakoof.com/t540/black-puffer-jacket-aw-21-364956-1634190927-2.jpg",
+        "image3": "https://images.bewakoof.com/t540/black-puffer-jacket-aw-21-364956-1634190933-3.jpg",
+        "image4": "https://images.bewakoof.com/t540/black-puffer-jacket-aw-21-364956-1634190940-4.jpg",
+        "image5": "https://images.bewakoof.com/t540/black-puffer-jacket-aw-21-364956-1634190945-5.jpg",
+        "desc": "Regular fit - Fits just right - not too tight, not too loose.",
+        "tribe": "100",
+        "discount": "63"
+    },
+    {
+        "id": "j8",
+        "name": "Navy Blue Plus Size Windcheater Jackets",
+        "price": "₹3349",
+        "image1": "https://images.bewakoof.com/t540/navy-blue-plus-size-windcheater-jackets-368673-1642433752-1.jpg",
+        "image2": "https://images.bewakoof.com/t540/navy-blue-plus-size-windcheater-jackets-368673-1642433756-2.jpg",
+        "image3": "https://images.bewakoof.com/t540/navy-blue-plus-size-windcheater-jackets-368673-1642433759-3.jpg",
+        "image4": "https://images.bewakoof.com/t540/navy-blue-plus-size-windcheater-jackets-368673-1642433766-4.jpg",
+        "image5": "https://images.bewakoof.com/t540/navy-blue-plus-size-windcheater-jackets-368673-1642433770-5.jpg",
+        "desc": "Oversized fit - Falls loosely on the body",
+        "tribe": "₹80",
+        "discount": "68"
+    },
+    {
+        "id": "j9",
+        "name": "Navy Blue Plus Size Windcheater Jackets",
+        "price": "3349",
+        "image1": "https://images.bewakoof.com/t540/navy-blue-plus-size-windcheater-jackets-368673-1642433752-1.jpg",
+        "image2": "https://images.bewakoof.com/t540/navy-blue-plus-size-windcheater-jackets-368673-1642433756-2.jpg",
+        "image3": "https://images.bewakoof.com/t540/navy-blue-plus-size-windcheater-jackets-368673-1642433759-3.jpg",
+        "image4": "https://images.bewakoof.com/t540/navy-blue-plus-size-windcheater-jackets-368673-1642433766-4.jpg",
+        "image5": "https://images.bewakoof.com/t540/navy-blue-plus-size-windcheater-jackets-368673-1642433770-5.jpg",
+        "desc": "Oversized fit - Falls loosely on the body",
+        "tribe": " ₹80",
+        "discount": "68"
+    },
+    {
+        "id": "j10",
+        "name": "Royal Blue Hoodie Puffer Jacket",
+        "price": "3299",
+        "image1": "https://images.bewakoof.com/t540/d-blue-puffer-jacket-aw-21-364933-1634198862-1.jpg",
+        "image2": "https://images.bewakoof.com/t540/d-blue-puffer-jacket-aw-21-364933-1634198869-2.jpg",
+        "image3": "https://images.bewakoof.com/t540/d-blue-puffer-jacket-aw-21-364933-1634198875-3.jpg",
+        "image4": "https://images.bewakoof.com/t540/d-blue-puffer-jacket-aw-21-364933-1634198881-4.jpg",
+        "image5": "https://images.bewakoof.com/t540/d-blue-puffer-jacket-aw-21-364933-1634198887-5.jpg",
+        "desc": "Regular fit - Fits just right - not too tight, not too loose.",
+        "tribe": "₹100",
+        "discount": "60"
+    },
+    {
+        "id": "j11",
+        "name": "Royal Blue Hoodie Puffer Jacket",
+        "price": "3299",
+        "image1": "https://images.bewakoof.com/t540/d-blue-puffer-jacket-aw-21-364933-1634198862-1.jpg",
+        "image2": "https://images.bewakoof.com/t540/d-blue-puffer-jacket-aw-21-364933-1634198869-2.jpg",
+        "image3": "https://images.bewakoof.com/t540/d-blue-puffer-jacket-aw-21-364933-1634198875-3.jpg",
+        "image4": "https://images.bewakoof.com/t540/d-blue-puffer-jacket-aw-21-364933-1634198881-4.jpg",
+        "image5": "https://images.bewakoof.com/t540/d-blue-puffer-jacket-aw-21-364933-1634198887-5.jpg",
+        "desc": "Regular fit - Fits just right - not too tight, not too loose.",
+        "tribe": "₹100",
+        "discount": "60"
+    }
+]
+}`
+}
+
+export default getData;
